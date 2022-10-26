8889a68af463e14836599f42449051793947129e
WE-DA-pluggg <79959576+SeYeol00@users.noreply.github.com>
2022-10-26 16:18:10 +0900

    Update ItemController.java

--- src/main/java/hello/upload/controller/ItemController.java
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -7,16 +7,21 @@ import hello.upload.domain.UploadFile;
 import hello.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.UriUtil;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -47,7 +52,7 @@ public class ItemController {
         itemRepository.save(item);
 
         redirectAttributes.addAttribute("itemId",item.getId());
-        return "redirect::/items/{itemsId}";
+        return "redirect:/items/{itemId}";
     }
 
     @GetMapping("/items/{id}")
@@ -56,4 +61,39 @@ public class ItemController {
         model.addAttribute("item",item);
         return "item-view";
     }
+
+
+    // 이미지를 다운로드하는 컨트롤러
+    // 파일 경로를 잡아서 실제 파일을 스트림으로 리턴해준다.
+    // 보안에 약간 취약
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        return new UrlResource("file:"+fileStore.getFullPath(filename));
+    }
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}",uploadFileName);
+
+
+        // 한글은 꺠질 수도 있으니 파일이름을 utf-8로 인코딩 필요
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+
+        // 여기서는 헤더를 넣어줘야한다.
+        // 이건 규약이다.
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,contentDisposition)
+                .body(resource);
+    }
+
+
 }
